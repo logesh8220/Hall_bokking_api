f41ac38523a119b37a8f650fa0bd92385c38d10a
logesh8220 <logeshthirumurugan@gmail.com>
2022-09-14 19:26:21 +0530

    change

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ app.use(express.json())
 
 
 app.get("/rooms", function (req, res) {
-    fs.readFile("./Data/rooms.json", function (err, data) {
+    fs.readFile("./rooms.json", function (err, data) {
         if (err) throw err;
         res.send(data)
     })
 });
 
 app.get("/bookedrooms", function (req, res) {
-    fs.readFile("./Data/booked-rooms.json", function (err, data) {
+    fs.readFile("./booked-rooms.json", function (err, data) {
         if (err) throw err;
         res.send(data)
     })
 });
 app.get("/bookedcustomers", function (req, res) {
-    fs.readFile("./Data/booked-customers.json", function (err, data) {
+    fs.readFile("./booked-customers.json", function (err, data) {
         if (err) throw err;
         res.send(data)
     })
